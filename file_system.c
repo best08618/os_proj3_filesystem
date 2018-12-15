8b5935182bc41d3087c98aab9dada49ae3b6bc66
best08618 <best08618@naver.com>
2018-12-15 20:39:57 +0900

    end data to phm

--- file_system.c
+++ file_system.c
@@ -13,11 +13,12 @@
 #include "fs.h"
 
 
-#define CHILDNUM 2
+#define CHILDNUM 1
 #define INDEXNUM 16
 #define FRAMENUM 32
 #define OPEN 0
 #define READ 1
+#define ENTRYNUM 2
 
 FILE* fptr;
 struct partition tf;//total file
@@ -34,14 +35,15 @@ int total_count = 0;
 pid_t pid[CHILDNUM];
 int pid_index;
 int fo_num = 0 ;
+int user_fo[ENTRYNUM];
 
 struct msgbuf{
 	long int  mtype;
 	int pid_index;
 	int msg_mode;
-	unsigned int  virt_mem;
-	char file_name[32];
-	int foqueue;
+	unsigned int  virt_mem[ENTRYNUM];
+	char file_name[ENTRYNUM][32];
+	int foqueue[ENTRYNUM];
 };
 
 typedef struct{
@@ -60,14 +62,14 @@ PHY_TABLE phy_mem [FRAMENUM];
 int fpl[32];
 int fpl_rear, fpl_front =0;
 
-unsigned int pageIndex;
-unsigned int virt_mem;
-unsigned int offset;
+unsigned int pageIndex[ENTRYNUM];
+unsigned int virt_mem[ENTRYNUM];
+unsigned int offset[ENTRYNUM];
 int foQueue[10];
 
 int msgq;
 int ret;
-int key = 0x12345;
+int key = 0x12346;
 struct msgbuf msg;
 
 
@@ -109,10 +111,11 @@ void first_inode() //size of data block, number of data blocks
 void root_file() //print root files
 {
 	/* ============================Read every file in root ================*/
+	printf("\n=======================Root dir files==========================\n");
 	for(int l = 0; l< block_num; l ++)
 	{
 		struct blocks* bp = &tf.data_blocks[block_store[l]];
-		printf("Read block : %d\n",block_store[l]);
+		//printf("\nRead block : %d\n",block_store[l]);
 		struct dentry* den;
 		char* sp = bp->d;
 		int read_size = 0 ;
@@ -121,16 +124,15 @@ void root_file() //print root files
 			den = (struct dentry *) sp;
 			sp += den->dir_length;
 			read_size += den->dir_length;
-			printf("file name : %s\n",*den->n_pad);
+			printf("%s ",*den->n_pad);
 		}
 	}
-
+	printf("\n=======================Initialization==========================\n"); 
 }
 
 int find_user_file(char* file_name)
 {
 	printf("FInd file name in find_user file function %s\n",file_name);
-	int find = 0;
 	for(int l = 0; l< block_num; l ++)
 	{
 		struct blocks* bp = &tf.data_blocks[block_store[l]];
@@ -149,64 +151,72 @@ int find_user_file(char* file_name)
 				printf("found %s, enter inode %d\n", *den->n_pad, den->inode);
 				user_node = den->inode;
 				printf("datablocks size : %dbytes\n", tf.inode_table[user_node].size);
-				find = 1;
-				break;
+				return user_node;
 			}
 
-		}
-		if (find == 1){
-			return user_node;
 		}
 	}
 
 }
 
 
-void open_file(char* file_name,int fo_num){
+void open_file(char(* file_name)[32],int* fo_num){
 
 	memset(&msg,0,sizeof(msg));
 	msg.mtype = IPC_NOWAIT;
 	msg.pid_index = i;
 	msg.msg_mode = OPEN;
-	char* sp = file_name;
-	int l = 0;
-	while(*sp){
-		msg.file_name[l] = file_name [l];
-		l++;
-		sp++;
+	char* sp ;
+	for(int j = 0 ; j < ENTRYNUM ; j++){
+		sp = file_name[j];
+		int l = 0;
+		while(*sp){
+			msg.file_name[j][l] = file_name[j][l];
+			l++;
+			sp++;
+		}
+		msg.foqueue[j] = fo_num[j];
 	}
-	msg.foqueue = fo_num;
 	ret = msgsnd(msgq, &msg, sizeof(msg),IPC_NOWAIT);
 	if(ret == -1)
 		perror("msgsnd error");
 
 }
 
-void read_file(int user_fo,unsigned int vm){
+void read_file(int* user_fo,unsigned int* vm){
 
 	memset(&msg,0,sizeof(msg));
 	msg.mtype = IPC_NOWAIT;
 	msg.pid_index = i;
 	msg.msg_mode = READ;
-	msg.foqueue = user_fo;
+	for(int l = 0; l < ENTRYNUM ; l++){
+		
+		msg.foqueue[l] = user_fo[l];
+		msg.virt_mem[l] = vm[l];
+	}
 	ret = msgsnd(msgq, &msg, sizeof(msg),IPC_NOWAIT);
 	if(ret == -1)
 		perror("msgsnd error");
 }
 
 void child_function(){
-	int file_num = 1;
-	char file_name[32];
+	
+	int file_num = 15;
+	char file_name[ENTRYNUM][32];
 	unsigned int addr;
-	int user_fo;
-	addr = (rand() %0x09)<<12;
-	addr |= (rand()%0xfff);
-	unsigned int vm = addr;
-	printf("VM : %x\n",vm);
-	sprintf(file_name,"file_%d",file_num);
-	printf("CHILD FUNCTION :FIle name : %s\n",file_name);
-	user_fo = fo_num;
-	open_file(file_name,fo_num++);
+	unsigned int vm[ENTRYNUM];
+
+	for (int l = 0 ; l < 2; l ++){
+		addr = (rand() %0x09)<<12;
+		addr |= (rand()%0xfff);
+		vm[l] = addr;
+		printf("VM : %04x\n",vm[l]);
+		sprintf(file_name[l],"file_%d",file_num++);
+		printf("Send FIle name : %s\n",file_name[l]);
+		user_fo[l] = fo_num++;
+	}
+
+	open_file(file_name,user_fo);
 	read_file(user_fo,vm);
 	exit(0);
 }
@@ -230,7 +240,7 @@ void initialize_phymem(){
 		phy_mem[l].data = NULL;
 	printf("Initialize phymem\n");
 }
-void find_user_data(int user_node)
+char* find_user_data(int user_node)
 {
 	struct inode* user_ptr;
 	user_ptr = &tf.inode_table[user_node];
@@ -255,6 +265,8 @@ void find_user_data(int user_node)
 		}
 		printf("data : %s\n", buffer);
 	}
+
+	return buffer;
 
 }
 
@@ -300,30 +312,62 @@ int main(int argc,char* argv[])
 				printf("pid index: %d\n", pid_index);
 				int mode = msg.msg_mode;
 				if( mode == OPEN){
-					char file_name[32];
-					int foqueue = msg.foqueue;
-					char* sp = msg.file_name;
-					int l = 0;
-					while(*sp){
-						file_name[l] = msg.file_name[l];
-						l++;
-						sp++;
+					char file_name[ENTRYNUM][32];
+					int foqueue[ENTRYNUM]; 
+					for(int j =0 ; j < ENTRYNUM ; j++){
+						foqueue[j]= msg.foqueue[j];
+						char* sp = msg.file_name[j];
+						int l = 0;
+						while(*sp){
+							file_name[j][l] = msg.file_name[j][l];
+							l++;
+							sp++;
+						}
+						printf("file name :%s\n",file_name[j]);
+						foQueue[foqueue[j]]=find_user_file(file_name[j]);
+						printf("inode:%d\n",foQueue[foqueue[j]]);
 					}
-
-					printf("file name :%s\n",file_name);
-					foQueue[foqueue]=find_user_file(file_name);
-					printf("inode:%d\n",foQueue[foqueue]);
 				}
 				else if (mode == READ)
 				{
-					printf("Read mode\n");
-					int foqueue = msg.foqueue;
-					find_user_data(foQueue[foqueue]);
-					return 0 ;				
+					for(int j = 0 ; j < ENTRYNUM ; j ++){
+						virt_mem[j] = msg.virt_mem[j];
+						offset[j] = virt_mem[j] & 0xfff;
+						pageIndex[j] = (virt_mem[j] & 0xf000) >> 12 ;
+						printf("Read mode\n");
+						int foqueue = msg.foqueue[j];
+						char* dp;
+						if(table[pid_index][pageIndex[j]].valid == 0) //if its invalid
+						{
+							if(fpl_front != fpl_rear)
+							{
+								table[pid_index][pageIndex[j]].pfn=fpl[fpl_front%FRAMENUM];
+								printf("VA %d -> PA %d\n", pageIndex[j],  table[pid_index][pageIndex[j]].pfn);
+								table[pid_index][pageIndex[j]].valid = 1;
+								fpl_front++;
+								dp= find_user_data(foQueue[foqueue]);
+								phy_mem[table[pid_index][pageIndex[j]].pfn].data = dp;
+							}
+							else
+							{
+								printf("full");
+								exit(0);
+							}
+
+						}
+						else if(table[msg.pid_index][pageIndex[j]].valid == 1)
+						{
+							printf("In the disk\n");
+						}
+					}
+					msgctl(msgq, IPC_RMID, NULL);
+					 return 0;
+						
 				}
 				
 			}
 			memset(&msg, 0, sizeof(msg));
+			//msgctl(msgq, IPC_RMID, NULL);
 		}
 		return 0;
 	}
